2ac9f34cc42019efa59ec42819049d2238244e72
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-06-01 04:06:36 +0900

    feat: title nodo

--- src/shared/wrap.html.tsx
+++ src/shared/wrap.html.tsx
@@ -4,7 +4,7 @@ export function wrapHtml<P = void>(jsx: (props?: P) => React.JSX.Element) {
   return (props?: P) => () => (
     <html lang="ko">
       <head>
-        <title>Echo</title>
+        <title>Nodo</title>
       </head>
       <body>
         <div id={'root'}>{jsx(props)}</div>
