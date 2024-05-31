3506a0bce47fcff3cf6c5acf22e389dd48c30645
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-06-01 01:43:10 +0900

    remove: vercel

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import { Elysia } from 'elysia'
 import { EchoController } from './echo/echo.controller.ts'
 import { html } from '@elysiajs/html'
+import { connectMongo } from './infra/connect.mongo.ts'
 
 import 'dotenv'
-import { connectMongo } from './infra/connect.mongo.ts'
-import node from '@elysiajs/node'
 
 const APP_PORT = Number(process.env.APP_PORT ?? 3000)
 
@@ -13,15 +12,10 @@ await connectMongo(
   process.env.DB_NAME as string,
 )
 
-const app = new Elysia()
+new Elysia()
   .use(html())
   .use(EchoController({ prefix: '/echo' }))
   .get('/', () => 'Hello Elysia')
-
-if (process.env.RUNTIME === 'node') {
-  app.use(node(APP_PORT))
-} else {
-  app.listen(APP_PORT, () => {
+  .listen(APP_PORT, () => {
     console.log(`App is running on http://localhost:${APP_PORT}`)
   })
-}
